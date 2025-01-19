6dea7011aa779cfcea0657f752477b8566132d07
ramiahmed68 <ramiahmet68@gmail.com>
2025-01-19 16:41:36 +0200

    fixed

--- src/main.js
+++ src/main.js
@@ -7,13 +7,10 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import "./sass/main.scss";
-import generateSitemap from "./sitemap";
 import { createHead } from "@vueuse/head";
 const head = createHead();
 
 library.add(fas);
-// use the craete site map file
-generateSitemap();
 
 createApp(App)
   .use(head)
