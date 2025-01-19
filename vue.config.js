1e50cefe5570660b7e7b21b83881ca8db5aa69c9
ramiahmed68 <ramiahmet68@gmail.com>
2025-01-19 19:17:27 +0200

    testing a sitemap

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new SitemapPlugin({
+        base: "https://example.com",
+        // paths: [
+        //   { path: '/', priority: 0.9 },
+        //   { path: '/about', priority: 0.8 },
+        //   // ... إضافة مسارات أخرى
+        // ]
+      }),
+    ],
+  },
 });
